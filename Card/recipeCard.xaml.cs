71b57a72fb236f8391771363742ccdedf7a12e2e
Jack <jackji1230@hotmail.com>
2012-11-10 10:51:17 -0500

    compilation errors

--- Card/recipeCard.xaml.cs
+++ Card/recipeCard.xaml.cs
@@ -15,7 +15,7 @@ using Windows.UI.Xaml.Navigation;
 
 // The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
 
-namespace recipecards
+namespace Recipe_Box
 {
     public sealed partial class MyUserControl1 : UserControl
     {
